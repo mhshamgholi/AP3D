150dff2fc6575d7c4b0f442a7eefc576964cb620
mohammadhasan shamgholi <mmshamgholi@gmail.com>
2022-06-20 17:26:20 +0430

    running row41

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ centers = np.array([0.1, 0.3, 0.5, 0.7, 0.9, 5, 13, 21, 29, 37])
 nbins = len(centers)
 
 hist_by_prof_edges = [0, 0.2, 0.4, 0.6, 0.8, 1] # 15, 30]
-use_dropout = True
+use_dropout = False
 use_hist = False #False
 use_just_last_bin = False
 concat_hist_max = False #False
@@ -17,23 +17,23 @@ use_pad_for_resnet18_Bottleneck3D = True # if use_resnet18 is False then this pa
 
 
 def get_spatial_transform_train(args):
-    return torchT.Compose([
-#                 torchT.Scale((int(args.height * 1.2), int(args.width * 1.2)), interpolation=3), #(args.height, args.width)
-#                 torchT.RandomCrop((args.height, args.width)),
-                torchT.Scale((int(args.height), int(args.width)), interpolation=3), #(args.height, args.width)
-                torchT.ColorJitter(brightness=.4), 
-                torchT.GaussianBlur(kernel_size=(5, 5), sigma=(0.1, 2)),
-#                 torchT.RandomRotation(degrees=(-15, 15)),
-#                 torchT.RandomHorizontalFlip(p=0.5),
-                torchT.ToTensor(),
-                ## @#$%^&*()_
-                torchT.RandomErasing(p=0.8, scale=(0.1, 0.2), ratio=(0.3, 3.3)),
-                ## @#$%^&*()_
-                torchT.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-            ])
 #     return torchT.Compose([
+# #                 torchT.Scale((int(args.height * 1.2), int(args.width * 1.2)), interpolation=3), #(args.height, args.width)
+# #                 torchT.RandomCrop((args.height, args.width)),
 #                 torchT.Scale((int(args.height), int(args.width)), interpolation=3), #(args.height, args.width)
-#                 torchT.RandomHorizontalFlip(p=0.5),
+#                 torchT.ColorJitter(brightness=.4), 
+#                 torchT.GaussianBlur(kernel_size=(5, 5), sigma=(0.1, 2)),
+# #                 torchT.RandomRotation(degrees=(-15, 15)),
+# #                 torchT.RandomHorizontalFlip(p=0.5),
 #                 torchT.ToTensor(),
-#                 torchT.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+#                 ## @#$%^&*()_
+#                 torchT.RandomErasing(p=0.8, scale=(0.1, 0.2), ratio=(0.3, 3.3)),
+#                 ## @#$%^&*()_
+#                 torchT.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
 #             ])
+    return torchT.Compose([
+                torchT.Scale((int(args.height), int(args.width)), interpolation=3), #(args.height, args.width)
+                torchT.RandomHorizontalFlip(p=0.5),
+                torchT.ToTensor(),
+                torchT.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ])
