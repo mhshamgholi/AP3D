b4f47210f665457265bcc2bd9366f68c9354551b
mohammadhasan shamgholi <mmshamgholi@gmail.com>
2022-06-21 12:57:49 +0430

    row41

--- train-mars-ap3d.sh
+++ train-mars-ap3d.sh
@@ -3,6 +3,6 @@
 
 
 #run in 213
-CUDA_VISIBLE_DEVICES=0 python train.py --root /mnt/File/shamgholi/datasets/ -d mars --arch ap3dres50 --gpu 0 --save_dir /mnt/File/shamgholi/projects/person_reid/AP3D/logs/row40 --train_batch 32 --test_batch 32 --lr 0.0003 --eval_step 10 #--weight_decay 0.001
+CUDA_VISIBLE_DEVICES=0 python train.py --root /mnt/File/shamgholi/datasets/ -d mars --arch ap3dres50 --gpu 0 --save_dir /mnt/File/shamgholi/projects/person_reid/AP3D/logs/row41 --train_batch 32 --test_batch 32 --lr 0.0003 --eval_step 10 #--weight_decay 0.001
 #--pretrain "./logs/log-mars-ap3d/best_model.pth.tar"
 #python test-all.py --root /home/guxinqian/data/ -d mars --arch ap3dres50 --gpu 0 --resume log-mars-ap3d
